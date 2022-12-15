613199b6357e7dbfc0e559c41d90d66ca67a938f
Lukas <lukaszdzie1908@gmail.com>
2022-12-15 16:09:45 +0100

    Zapis dancyh

---
+++ API-ArchitekturaAplikacji-ZapisDanych/src/main/java/arch/company/Company.java
@@ -0,0 +1,25 @@
+package arch.company;
+
+import arch.joboffer.JobOffer;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+@Getter
+@Setter
+@Entity
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String city;
+    private Integer employees;
+    private String telephone;
+    private String email;
+    @OneToMany(mappedBy = "company")  // relacja zwrotna i wskazujemy nazwe pola company
+    private List<JobOffer> jobOffers = new ArrayList<>();
+}
