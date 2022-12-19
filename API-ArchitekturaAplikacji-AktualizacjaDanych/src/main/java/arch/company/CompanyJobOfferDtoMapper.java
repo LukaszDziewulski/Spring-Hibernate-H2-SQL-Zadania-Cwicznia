60688797647795cd04512fe9dfde727cce4d8805
Lukas <lukaszdzie1908@gmail.com>
2022-12-19 11:30:36 +0100

    Aktualizacja dancyh

---
+++ API-ArchitekturaAplikacji-AktualizacjaDanych/src/main/java/arch/company/CompanyJobOfferDtoMapper.java
@@ -0,0 +1,17 @@
+package arch.company;
+
+import arch.joboffer.JobOffer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompanyJobOfferDtoMapper {
+    CompanyJobOfferDto map(JobOffer jobOffer) {
+        CompanyJobOfferDto dto = new CompanyJobOfferDto();
+        dto.setId(jobOffer.getId());
+        dto.setTitle(jobOffer.getTitle());
+        dto.setLocation(jobOffer.getLocation());
+        dto.setMinSalary(jobOffer.getMinSalary());
+        dto.setMaxSalary(jobOffer.getMaxSalary());
+        return dto;
+    }
+}
---
+++ API-ArchitekturaAplikacji-AktualizacjaDanych/src/main/java/arch/joboffer/JobOfferService.java
@@ -0,0 +1,30 @@
+package arch.joboffer;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class JobOfferService {
+    private JobOfferRepository jobOfferRepository;
+    private JobOfferDtoMapper jobOfferDtoMapper;
+
+    public JobOfferService(JobOfferRepository jobOfferRepository, JobOfferDtoMapper jobOfferDtoMapper) {
+        this.jobOfferRepository = jobOfferRepository;
+        this.jobOfferDtoMapper = jobOfferDtoMapper;
+    }
+
+    Optional<JobOfferDto> getOfferById(Long id) {
+        return jobOfferRepository.findById(id).map(jobOfferDtoMapper::map);
+    }
+
+    JobOfferDto saveOffer(JobOfferDto jobOfferDto){
+        JobOffer jobOfferToSave = jobOfferDtoMapper.map(jobOfferDto);
+        jobOfferToSave.setDateAdded(LocalDateTime.now());
+        JobOffer saveJobOffer = jobOfferRepository.save(jobOfferToSave);
+        return  jobOfferDtoMapper.map(saveJobOffer);
+
+    }
+
+}
