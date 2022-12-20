825044181bbff58381c130d03795998a8bb0941d
Lukas <lukaszdzie1908@gmail.com>
2022-12-20 19:47:06 +0100

    Wprowadzenie

---
+++ SpringSecurity--Wprowadzenie/src/main/java/sec/HomeControler.java
@@ -0,0 +1,12 @@
+package sec;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeControler {
+    @GetMapping("/")
+    String home() {
+        return "index";
+    }
+}
