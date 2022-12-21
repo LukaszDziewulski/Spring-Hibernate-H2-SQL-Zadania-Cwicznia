7653ed80161711b58ac785f6e6589242338e91dd
Lukas <lukaszdzie1908@gmail.com>
2022-12-21 18:48:09 +0100

    Wbudowane Zrodlo Uzytkownikow

---
+++ SpringSecurity-DostepDoKontekstuBezpieczenstwa/src/main/java/sec/web/HomeControler.java
@@ -0,0 +1,27 @@
+package sec.web;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import sec.message.WelcomeMessageService;
+
+@Controller
+public class HomeControler {
+
+    WelcomeMessageService welcomeMessageService;
+
+    public HomeControler(WelcomeMessageService welcomeMessageService) {
+        this.welcomeMessageService = welcomeMessageService;
+    }
+
+    @GetMapping("/")
+    String home(@RequestParam(defaultValue =  "en")String lang, Model model) {
+        String welcomeMessage = welcomeMessageService.getWelcomeMessage(lang);
+        model.addAttribute("welcomeMessage",welcomeMessage);
+        return "index";
+    }
+}
