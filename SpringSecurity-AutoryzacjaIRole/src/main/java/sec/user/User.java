9f62a8dbbd1cb35219fea13cb5e89d3aaff25962
Lukas <lukaszdzie1908@gmail.com>
2022-12-23 11:43:30 +0100

    Autoryzacja i Role

---
+++ SpringSecurity-AutoryzacjaIRole/src/main/java/sec/user/User.java
@@ -0,0 +1,30 @@
+package sec.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+@Data
+@Entity
+@Table(name = "application_user") // Słowo "USER" jest zarezerwowanym słowem standardu SQL i słowem kluczowym w H2,
+// dlatego dodałem adnotację @Table, wskazując tabelę o nazwie "application_user".
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER) // Domyślnie kolekcje stanowiące relację jeden do wielu i wiele do
+    // wielu wczytywane są leniwie. Z tego powodu dodałem atrybut fetch = FetchType.EAGER,
+    // dzięki któremu role zawsze będą doczytane zachłannie wraz z użytkownikiem
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+
+    )
+    private Set<UserRole> roles = new HashSet<>();
+}
---
+++ SpringSecurity-AutoryzacjaIRole/src/main/java/sec/web/LoginControler.java
@@ -0,0 +1,12 @@
+package sec.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginControler {
+    @GetMapping("/login")
+    String loginForm(){
+        return "login-form";
+    }
+}
