f0c28bae141c78844234875c3b379964b249edcc
Lukas <lukasz.dziewulski.kontakt@gmail.com>
2023-01-18 12:34:50 +0100

    Ochrona Przed CSRF

---
+++ SpringSecurity-OchronaPrzedCSRF/src/main/java/sec/user/UserRepository.java
@@ -0,0 +1,13 @@
+package sec.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    List<User> findAllUsersByRoles_Name(String role);
+    void  deleteByEmail(String email);
+
+}
