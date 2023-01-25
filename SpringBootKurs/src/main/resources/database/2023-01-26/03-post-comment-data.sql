db028c3512d01542c2ee377d76b13981e52efe88
LukaszDziewulski <lukasz.dziewulski.kontakt@gmail.com>
2023-01-25 18:46:59 +0100

    Spring Boot #09 - Liquibase

--- SpringBootKurs/src/main/resources/database/2023-01-26/03-post-comment-data.sql
+++ SpringBootKurs/src/main/resources/database/2023-01-26/03-post-comment-data.sql
@@ -1,3 +1,5 @@
+--liquibase formatted sql
+--changeset dziewuslkil:1
 insert into post(id, title, content,created) values (100,'Test post 1', 'Content 1', '2020-07-11T14:26:43.612121200');
 insert into post(id, title, content,created) values (200,'Test post 2', 'Content 1', '2020-08-11T14:26:43.612121200');
 insert into post(id, title, content,created) values (300,'Test post 3', 'Content 1', '2020-09-11T14:26:43.612121200');
